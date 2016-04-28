7a3aaa1fb3770c84a1612fda54bfe603d2ea146c
qwerty2017 <gbtecno@gmail.com>
2016-04-28 14:49:45 +0200

    added search box

--- dataview.py
+++ dataview.py
@@ -11,6 +11,8 @@ ad = 'DESC'
 root = ''
 tablename_1 = ''
 dbname_1 = ''
+searchbox = {}
+search = ''
 
 class AutoScrollbar(Scrollbar):
     # a scrollbar that hides itself if it's not needed.  only
@@ -27,15 +29,9 @@ class AutoScrollbar(Scrollbar):
     def place(self, **kw):
         raise TclError, "cannot use place with this widget"
     
-def grid_view(frame, mTable, mDBname):     
-    #global ad     
-    #if ad == 'DESC': ad = 'ASC'
-    #else: ad = 'DESC'   
-    table = dbutils.get_table_metadata(mDBname, mTable, sort, ad)     
-    #col_number = table[0]
-    col_names = table[0]
-    #col_extensions =table[2]
-    #row_num = table[3]
+def grid_view(frame, mTable, mDBname):        
+    table = dbutils.get_table_metadata(mDBname, mTable, sort, ad)   
+    col_names = table[0]   
     rows = table[1]        
     entry = {}   
     label = {}
@@ -43,36 +39,56 @@ def grid_view(frame, mTable, mDBname):
     buttons.clear() 
     button_num = 0
     global cols
+    global searchbox  
+    global search       
     cols_num = 0
     l = 0
+    space = Label(frame, text='', pady = 5)
+    space.grid(row=0, column = 0, sticky= W) 
+    b = Button(frame, text='Add Record', bd=1)
+    b.grid(row=1, column=l+1, sticky=W)
+    b.bind( "<Button-1>", new_record)
+    b = Button(frame, text='Search', bd=1)
+    b.grid(row=1, column=l+2, sticky=W)
+    b.bind( "<Button-1>", filter_records)
+    e = Entry(frame)
+    e.grid(row=1, column=l+3, sticky=W)
+    searchbox['search'] = e        
+    space = Label(frame, text='', pady = 5)
+    space.grid(row=2, column = 0, sticky= W) 
     for name in col_names:
         b = Button(frame, text=name, bd=0)
-        b.grid(row=0, column=l+1, sticky=W)
+        b.grid(row=3, column=l+1, sticky=W)
         cols[b] = cols_num
         cols_num += 1
         label[name] = b        
         l += 1   
         b.bind("<Button-1>", sort_by)              
-    r = 0
-    for row in rows:                        
+    r = 0  
+    #Rows start rs
+    rs = 4 
+    filtered_rows = []      
+    for rw in rows:
+        if (search in rw) or (len(search) < 1): filtered_rows.append(rw)    
+    for row in filtered_rows:                               
         b = Button(frame, text= r+1)        
-        b.grid(row=r+1, column=0, sticky = W, padx = 2)         
+        b.grid(row=r+rs, column=0, sticky = W, padx = 2)         
         buttons[b] = button_num        
         button_num += 1
         b.bind( "<Button-1>", get_record)               
         c = 0      
         for col in row:          
             e = Entry(frame)
-            e.grid(row=r+1, column=c+1)
+            e.grid(row=r+rs, column=c+1)
             entry[row] = e
-            if rows[r][c] == None:
+            if filtered_rows[r][c] == None:
                 e.insert(0,'')
             else:
-                e.insert(0,rows[r][c]) 
+                e.insert(0,filtered_rows[r][c]) 
             e.configure(state='readonly')                      
             c = c + 1
         r = r + 1
-         
+        
 def record_view(frame, mTable, mDBname, record=0): 
     global ad     
     #if ad == 'DESC': ad = 'ASC'
@@ -98,16 +114,16 @@ def record_view(frame, mTable, mDBname, record=0):
             e.current(0)
             e.grid(row=i, column=2, columnspan=3, sticky=W)
             box[name] = e
-        except:    
+        except:                    
             e = Entry(frame)
             e.grid(row=i, column=1, columnspan=3)
             entry[name] = e                
-            if rows[record][i] == None: 
+            if (rows[record][i] == None) or (record == -1): 
                 e.insert(0,'')
             else:
                 e.insert(0,rows[record][i])
-                if (pk == name):
-                    e.configure(state='readonly')        
+            if (pk == name):
+                e.configure(state='readonly')        
         lb = Label(frame, text=name, pady = 5)
         lb.grid(row=i, column=0, sticky=W)
         label[name] = lb        
@@ -175,22 +191,26 @@ def record_view(frame, mTable, mDBname, record=0):
             tkMessageBox.showinfo("Delete record", "Record deleted")
             scrolled_view(root,mDBname,mTable,'g',0)
         except Exception, err:
-            tkMessageBox.showerror("Error", err)       
-
-    b_avanti = Button(frame, text="->", command=forward)
-    b_avanti.grid(row=i, column = 3, sticky = W)
-    b_indietro = Button(frame, text="<-", command=back)
-    b_indietro.grid(row=i, column = 2, sticky = E)
-    
-    space = Label(frame, text='', pady = 5)
-    space.grid(row=i+1, column = 0, sticky= W)   
-    b_create = Button(frame, text="New", command=create, width = 8)
-    b_create.grid(row=i+2, column =1, sticky = N)
-    b_update = Button(frame, text="Update", command=update, width = 8)
-    b_update.grid(row=i+2, column =2, sticky = N)
-    b_delete = Button(frame, text="Delete", command=delete, width = 8)
-    b_delete.grid(row=i+2, column =3, sticky = N)   
-    
+            tkMessageBox.showerror("Error", err)   
+            
+    if (record == -1):
+        space = Label(frame, text='', pady = 5)
+        space.grid(row=i+1, column = 0, sticky= W)   
+        b_create = Button(frame, text="Save", command=create, width = 8)
+        b_create.grid(row=i+2, column =1, sticky = N)
+        
+    else:
+        b_avanti = Button(frame, text="->", command=forward)
+        b_avanti.grid(row=i, column = 3, sticky = W)
+        b_indietro = Button(frame, text="<-", command=back)
+        b_indietro.grid(row=i, column = 2, sticky = E)    
+        space = Label(frame, text='', pady = 5)
+        space.grid(row=i+1, column = 0, sticky= W)       
+        b_update = Button(frame, text="Update", command=update, width = 8)
+        b_update.grid(row=i+2, column =2, sticky = N)
+        b_delete = Button(frame, text="Delete", command=delete, width = 8)
+        b_delete.grid(row=i+2, column =3, sticky = N)  
+        
 def scrolled_view(main, dbname, tablename, t, record):
     
     def on_mousewheel(event):
@@ -245,17 +265,35 @@ def get_record(event):
     record = buttons[event.widget]       
     scrolled_view(root,dbname_1, tablename_1,'r', record)
     
+def new_record(event):
+    ## free up memory 
+    for item in root.grid_slaves():
+        item.destroy()          
+    scrolled_view(root,dbname_1, tablename_1,'r', -1)
+    
+    
 def sort_by(event):
     ## free up memory 
     for item in root.grid_slaves():
         item.destroy() 
     global cols    
-    global sort
+    global sort      
     sort = cols[event.widget]  
     global ad     
     if ad == 'DESC': ad = 'ASC'
     else: ad = 'DESC'             
     scrolled_view(root,dbname_1, tablename_1,'g',sort)
+    
+def filter_records(event):
+    global searchbox 
+    global search
+    ### Set filter
+    search = searchbox['search'].get()
+    ## free up memory 
+    for item in root.grid_slaves():
+        item.destroy() 
+    ### Recreate grid with filter if exist               
+    scrolled_view(root,dbname_1, tablename_1,'g',0)
     
 
 
